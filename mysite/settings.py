845a7897438bdc85f4fd232cd328b3e446fa7ef4
nathaniel-alvin <alvinparali@gmail.com>
2022-06-28 18:22:48 +0700

    update settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')+fhp5@@6e^dwo9n$msq6rv&&%q#_t(6*ouhh6q8+k*+lv-a2$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'characternames-backend.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'characternames-backend.herokuapp.com', 'https://xcoffeevin.herokuapp.com/']
 
 # Application definition
 INSTALLED_APPS = [
@@ -145,5 +145,6 @@ CORS_ORIGIN_WHITELIST = (
     'https://localhost:3000',
     'http://localhost:3000',
     'https://herosname-react-frontend.vercel.app',
-    'https://lyfe-is-pain.herokuapp.com'
+    'https://lyfe-is-pain.herokuapp.com',
+    'https://xcoffeevinfe.herokuapp.com/'
 )
